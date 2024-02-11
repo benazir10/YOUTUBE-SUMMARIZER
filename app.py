5c4f1e477d563743b97c68a89a01efc785fceb5c
benazir10 <128906117+benazir10@users.noreply.github.com>
2024-02-11 16:42:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,22 @@
 from flask import Flask, request
 from youtube_transcript_api import YouTubeTranscriptApi
 from transformers import pipeline
+
 app = Flask(__name__)
+
 @app.get('/summary')
 def summary_api():
-  url = request.args.get('url', '')
-video_id = url.split('=')[1]
-summary = get_summary(get_transcript(video_id))
-return summary, 200
+    url = request.args.get('url', '')
+    video_id = url.split('=')[1]
+    summary = get_summary(get_transcript(video_id))
+    return summary, 200
+  
 def get_transcript(video_id):
+  
     transcript_list = YouTubeTranscriptApi.get_transcript(video_id)
     transcript = ' '.join([d['text'] for d in transcript_list])
     return transcript
+  
 def get_summary(transcript):
     summariser = pipeline('summarization')
     summary = ''
@@ -19,6 +24,7 @@ def get_summary(transcript):
         summary_text = summariser(transcript[i*1000:(i+1)*1000])[0]['summary_text']
         summary = summary + summary_text + ' '
     return summary
+  
 if __name__ == '__main__':
    app.run()
 
