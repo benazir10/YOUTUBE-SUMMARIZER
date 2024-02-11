711bb37057e9f1a0a2d0b6d798ac4d59714a41b8
benazir10 <128906117+benazir10@users.noreply.github.com>
2024-02-11 16:36:40 +0530

    Route Definition
    Defines a route ' /summary ' for handling HTTP GET requests

--- app.py
+++ app.py
@@ -2,4 +2,7 @@ from flask import Flask, request
 from youtube_transcript_api import YouTubeTranscriptApi
 from transformers import pipeline
 app = Flask(__name__)
+@app.get('/summary')
+def summary_api():
+
 
